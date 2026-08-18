cce72d9c42c0ad6dee931b79e09955723738e169
gumiInst <nguyenhatrung411@gmail.com>
2026-08-18 10:45:52 +0900

    Enhance firmware configuration and logging. Added RTS/DTR settings, upload speed, and increased debug level in platformio.ini. Updated main.cpp to use ESP-IDF logging instead of Serial for better log management.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
+#include "esp_log.h"
+
+static const char *TAG = "main";
 
 void setup() {
   Serial.begin(115200);
+  delay(500);  
+  ESP_LOGI(TAG, "Setup done");
 }
 
 void loop() {
-  Serial.println("Hello, World!");
+  ESP_LOGI(TAG, "Hello, World!");
   delay(1000);
 }
